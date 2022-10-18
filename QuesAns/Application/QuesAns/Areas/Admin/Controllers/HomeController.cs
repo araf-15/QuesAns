6faf73a715fd8cbbce02fe708b8898022428f87a
araf-15 <hasan.araf15@gmail.com>
2022-10-19 00:44:56 +0600

    Validated Login operation with session value

--- QuesAns/Application/QuesAns/Areas/Admin/Controllers/HomeController.cs
+++ QuesAns/Application/QuesAns/Areas/Admin/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using QuesAns.Areas.Admin.Models;
 using System;
@@ -12,7 +13,15 @@ namespace QuesAns.Areas.Admin.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            if(HttpContext.Session.GetString("Id") != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account", new { area = "" });
+            }
+
         }
 
         public IActionResult GetUsers()
